165075273c0f988ff3996a4b6defa3859cca174c
Dchans <135418178+Dchans@users.noreply.github.com>
2023-07-08 22:33:28 +0530

    Update settings.py

--- dchan/settings.py
+++ dchan/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@_%_e^eukj!=g2t6r8&n(+vb+1i%ea0ku4v2&mqehif@@5o*@1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["34.100.175.152"]
+ALLOWED_HOSTS = ["34.93.143.191","dchans.com"]
 
 
 # Application definition
